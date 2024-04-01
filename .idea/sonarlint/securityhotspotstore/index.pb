b1b9838cd8572df957932dfe728b67efabef09a3
Oleksandr Savenets <134528120+OleksandrSaven@users.noreply.github.com>
2024-04-01 16:45:32 +0300

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,14 @@
+
+h
+8app/src/main/java/com/example/pokedexapp/MainActivity.kt,9\4\94c139c977cebd66ec8ce6bb42007ba657842d0d
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+‚
+Rapp/src/main/java/com/example/pokedexapp/ui/theme/compose/navigation/BottomItem.kt,2\8\284ba2d1a5d47fbaf0ff821b2fb85c92589e67d5
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+H
+.github/workflows/ci.yml,8\9\899ce9c202bf7bb5480e72836c3edc773c9c4244
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.3" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    application
+    id("io.gitlab.arturbosch.detekt") version "1.23.3"
+
+}
+
+detekt {
+    buildUponDefaultConfig = true // preconfigure defaults
+    allRules = false // activate all available (even unstable) rules.
+    baseline = file("$projectDir/config/baseline.xml") // a way of suppressing issues before introducing detekt
+}
